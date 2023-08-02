febff7cded63a053875a1ce0cde399c6560488de
David Stripeik <dpstrip@gmail.com>
2023-08-02 11:05:20 -0500

    env variables and lookup vpc info

--- lib/load_balancer_code-stack.ts
+++ lib/load_balancer_code-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
 
 export class LoadBalancerCodeStack extends cdk.Stack {
+
+  private readonly vpcId = 'vpc-0e6be85a4258eb935';
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LoadBalancerCodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = ec2.Vpc.fromLookup(this, "VPC", {vpcId: this.vpcId});
+    
   }
 }
